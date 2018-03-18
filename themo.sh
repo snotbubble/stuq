2ce967826e1602f7f031cdfcedd8066a4a92347b
Craig Brown <36560021+snotbubble@users.noreply.github.com>
2018-03-19 03:44:43 +1100

    Add files via upload

--- themo.sh
+++ themo.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 while true
 do
-    HUH=$(hostname)
-    loc=`ls -1 ./pending/$HUH/*.sh 2>/dev/null | wc -l`
-    if [ $loc != 0 ]; then
-        for file in ./pending/$HUH/*.sh
-		do
-			echo 'file=' $file
-			if [ -f $file ]; then
-				chmod u+x $file
-				src=$(readlink --canonicalize $file)
-				fnm=$(basename $src)
-				mv $src ./running/$HUH/$fnm
-				/bin/bash ./running/$HUH/$fnm
-				mv ./running/$HUH/$fnm ./done/$HUH/$fnm
-				break
-			fi
+	iam=`hostname -s`
+	loc=`ls -1 ./pending/$iam/*.sh 2>/dev/null | wc -l`
+	if [ $loc != 0 ]; then
+		for file in ./pending/$iam/*.sh
+			do
+				if [ -f $file ]; then
+					chmod u+x $file
+					src=$(readlink --canonicalize $file)
+					fnm=$(basename $src)
+					mkdir ./running/$iam/
+		   			mv $src ./running/$iam/$fnm
+					/bin/bash ./running/$iam/$fnm
+					mv ./running/$iam/$fnm ./done/$fnm
+		   			break
+				fi
 		done
 	else
 		for file in ./pending/*.sh
-		do
-			if [ -f $file ]; then
-				chmod u+x $file
-				src=$(readlink --canonicalize $file)
-				fnm=$(basename $src)
-		   		mv $src ./running/$HUH/$fnm
-				/bin/bash ./running/$HUH/$fnm
-				mv ./running/$HUH/$fnm ./done/$fnm
-		   		break
-			fi
-		done
+			do
+				if [ -f $file ]; then
+					chmod u+x $file
+					src=$(readlink --canonicalize $file)
+					fnm=$(basename $src)
+					mkdir ./running/$iam/
+					mv $src ./running/$iam/$fnm
+					/bin/bash ./running/$iam/$fnm
+					mv ./running/$iam/$fnm ./done/$fnm
+					break
+				fi
+			done
 	fi
 	file=0
 	src=0
@@ -37,3 +38,4 @@ do
 	loc=0
 	sleep 2
 done
+
